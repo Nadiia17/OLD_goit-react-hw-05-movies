654233dc40b4ef91edec218a94075d1bc5ec7ffc
Nadiia17 <nababiy@gmail.com>
2023-10-29 16:29:04 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
-import { AppContainer } from './Layout';
+// import { useEffect, useState } from 'react';
+// import toast, { Toaster } from 'react-hot-toast';
+// import { AppContainer } from './Layout';
 
-import { searchByQuery } from './api';
+// import { searchByQuery } from './api';
 
-import { Loader } from './Loader/Loader';
+// import { Loader } from './Loader/Loader';
 
 export const App = () => {
   return <p>Hello</p>;
